0da15844b77e1c21faa3cdf6befe6967493f1598
aliksandrin <foxa3360>
2017-11-29 23:06:17 +0200

    first var

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task36.task3605;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.TreeSet;
@@ -10,5 +11,21 @@ import java.util.TreeSet;
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        TreeSet<String> set = new TreeSet<>();
+
+        int symbol = 0;
+        while ((symbol = bufferedReader.read()) != -1){
+            String letter = ("" + (char)symbol).toLowerCase();
+            if (letter.equals("[A-Za-z]") && !set.contains(letter)) set.add(letter);
+        }
+
+        String output = "";
+        for (int i = 0; i < 5; i++){
+            if (set.isEmpty()) break;
+            output += set.pollFirst();
+        }
+        //    /home/li/Downloads/subscription-form(copy).txt
     }
 }
