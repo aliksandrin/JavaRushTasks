d24e993a9d25b7ff4ad50f7af384d3f5ec41d72b
aliksandrin <foxa3360>
2017-11-30 00:08:28 +0200

    not valid but beautiful

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -1,31 +1,29 @@
 package com.javarush.task.task36.task3605;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 /* 
 Использование TreeSet
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        File file = new File(args[0]);
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-        TreeSet<String> set = new TreeSet<>();
-
-        int symbol = 0;
-        while ((symbol = bufferedReader.read()) != -1){
-            String letter = ("" + (char)symbol).toLowerCase();
-            if (letter.equals("[A-Za-z]") && !set.contains(letter)) set.add(letter);
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bufferedReader= Files.newBufferedReader(Paths.get(args[0]), StandardCharsets.UTF_8);
+        while (bufferedReader.ready()){
+            sb.append(bufferedReader.readLine().toLowerCase().replaceAll("[^A-Za-z]", ""));
         }
+        TreeSet<Character> set = new TreeSet<>(sb.toString().chars().mapToObj(e->(char)e).collect(Collectors.toList()));
+        set.stream().limit(5).forEach(System.out::print);
 
-        String output = "";
-        for (int i = 0; i < 5; i++){
-            if (set.isEmpty()) break;
-            output += set.pollFirst();
-        }
+
+
+
         //    /home/li/Downloads/subscription-form(copy).txt
     }
 }
